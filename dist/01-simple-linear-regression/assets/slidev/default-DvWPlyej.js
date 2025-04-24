002a3995cfe8e9a03003f08130d603ea3c731939
Ethan Goh <7086cmd@gmail.com>
2025-04-24 13:04:50 +0800

    chore: remove dist

--- dist/01-simple-linear-regression/assets/slidev/default-DvWPlyej.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-DXMGTi0W.js";import{_ as r}from"../index-yC6A4fbV.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
